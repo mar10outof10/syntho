b5ed7d548361392f313dac2e4782d68737afa93b
Martin-DeckerMed <105661802+Martin-DeckerMed@users.noreply.github.com>
2022-10-18 21:09:45 -0400

    Updated relative imports, backbone setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import './App.css';
+import SynthoMain from 'components/SynthoMain'
 
 function App() {
   return (
     <div className="App">
+      <div className="pageContainer">
+        <div className="contentContainer">
+          <SynthoMain />
+        </div>
+      </div>
       
     </div>
   );
---
+++ src/components/SynthoMain.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const SynthoMain = () => {
+  return (
+    <div style={{
+      height: '200px',
+      width: '200px',
+      backgroundColor: 'red',
+    }}>
+
+    </div>
+  )
+}
+
+export default SynthoMain
