697671d5d55f13c046b85df94f2867c32d8ce47f
Martin-DeckerMed <105661802+Martin-DeckerMed@users.noreply.github.com>
2022-10-24 18:49:32 -0400

    Stylized simple track list

--- src/components/UserInterface/TrackList.js
+++ src/components/UserInterface/TrackList.js
@@ -1,12 +1,21 @@
 import React from "react";
 
 const Track = ({track}) => {
-  <div className={`trackList_track trackList_track${track} rounded-square`}></div>
+  return (
+    <div className={`trackList__track trackList_track${track} rounded-square`}>{track}</div>
+  )
 }
 
 const TrackList = () => {
   return (
-    <div className="trackList"></div>
+    <div className="trackList">
+      <Track track={1}/>
+      <Track track={2}/>
+      <Track track={3}/>
+      <Track track={4}/>
+      <Track track={5}/>
+      <Track track={6}/>
+    </div>
   )
 }
 
