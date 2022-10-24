4e3f29e0fe731c337fdd2c9529265b98158ddbd8
Martin-DeckerMed <105661802+Martin-DeckerMed@users.noreply.github.com>
2022-10-24 18:21:44 -0400

    Added UI elements for tempo display

--- src/components/UserInterface/TempoDisplay.js
+++ src/components/UserInterface/TempoDisplay.js
@@ -3,8 +3,9 @@ import React from "react";
 const TempoDisplay = () => {
   return (
     <div className="tempoDisplay">
+      <div className="triangle-left"></div>
       <div className="tempoDisplay__digital">
-        
+      <div className="triangle-right"></div>
       </div>
     </div>
   )
--- src/components/UserInterface/TrackList.js
+++ src/components/UserInterface/TrackList.js
@@ -1,5 +1,9 @@
 import React from "react";
 
+const Track = ({track}) => {
+  <div className={`trackList_track trackList_track${track} rounded-square`}></div>
+}
+
 const TrackList = () => {
   return (
     <div className="trackList"></div>
