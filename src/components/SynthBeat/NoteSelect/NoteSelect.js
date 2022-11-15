a070ee4cf38a57d5d741a2d5fc2ba5c6099c9056
Martin-DeckerMed <105661802+Martin-DeckerMed@users.noreply.github.com>
2022-11-15 12:27:02 -0500

    Setup note select backbone

--- src/components/SynthBeat/NoteSelect/NoteSelect.js
+++ src/components/SynthBeat/NoteSelect/NoteSelect.js
@@ -1,9 +1,40 @@
 import React from "react";
 
+const notes = ["Ab", "Bb", "C", "Db", "Eb", "F", "Gb"];
+const octaves = ["1", "2", "3", "4", "5", "6"];
+
+const currNote = notes[0];
+const currOctave = octaves[0];
+
+const NoteColumn = () => {
+  return (
+    <div className="noteDropdown__noteColumn">
+      {notes.map((note, index) => (
+        <div>
+          {note}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+const OctaveColumn = () => {
+  return (
+    <div className="noteDropdown__octaveColumn">
+      {octaves.map((octave, index) => (
+        <div>
+          {octave}
+        </div>
+      ))}
+    </div>
+  );
+};
+
 const NoteSelect = () => {
   return (
     <div className='noteSelect'>
-      
+      <NoteColumn />
+      <OctaveColumn />
     </div>
   )
 }
