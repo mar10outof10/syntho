aa3cd9fbfbaf0557db42e1880af03ed5345e2572
Martin-DeckerMed <105661802+Martin-DeckerMed@users.noreply.github.com>
2022-11-15 13:24:50 -0500

    Implemented octave item styling

--- src/components/SynthBeat/NoteSelect/NoteSelect.js
+++ src/components/SynthBeat/NoteSelect/NoteSelect.js
@@ -1,10 +1,14 @@
 import React from "react";
+import classNames from "classnames";
 
 const notes = ["Ab", "Bb", "C", "Db", "Eb", "F", "Gb"];
 const octaves = ["1", "2", "3", "4", "5", "6"];
 
 const currNote = notes[0];
 const currOctave = octaves[0];
+
+const selectedNote = notes[1];
+const selectedOctave = octaves[1];
 
 const NoteColumn = () => {
   return (
@@ -22,8 +26,15 @@ const OctaveColumn = () => {
   return (
     <div className="noteSelect__octaveColumn">
       {octaves.map((octave, index) => {
+        
+        const octaveItemClass = [
+          'noteSelect__octaveColumnItem',
+          {noteSelect__octaveColumnItem__current: octave === currOctave},
+          {noteSelect__octaveColumnItem__selected: octave === selectedOctave}
+        ];
+
         return (
-          <div className="noteSelect__octaveColumnItem">
+          <div className={classNames(octaveItemClass)}>
             {octave}
           </div>
         )
