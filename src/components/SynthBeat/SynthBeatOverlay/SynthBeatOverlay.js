4a392c1dd07835c1de90d84031839658b305e166
mar10outof10 <78447684+mar10outof10@users.noreply.github.com>
2022-12-05 14:13:57 -0500

    Adding options skeleton to overlay

--- src/components/SynthBeat/SynthBeatOverlay/SynthBeatOverlay.js
+++ src/components/SynthBeat/SynthBeatOverlay/SynthBeatOverlay.js
@@ -1,11 +1,46 @@
 import React, { useReducer } from "react";
 import NoteSelect from './NoteSelect';
 
+const ConfirmButton = ({beatState, beatDispatch}) => {
+  return (
+    <div className='synthBeatOverlay__button confirmButton'>
+
+    </div>
+  )
+}
+
+const CancelButton = ({beatState, beatDispatch}) => {
+  return (
+    <div className='synthBeatOverlay__button cancelButton'>
+
+    </div>
+  )
+}
+
+const ClearButton = ({beatState, beatDispatch}) => {
+  return (
+    <div className='clearButton__container'>
+      <div className='clearButton__text'>
+        
+      </div>
+      <div className='clearButton__checkBox'>
+
+      </div>
+    </div>
+  )
+}
+
 const SynthBeatOverlay = ({beatState, beatDispatch}) => {
   return (
     <div className='synthBeatOverlay'>
       <NoteSelect beatState={beatState} beatDispatch={beatDispatch} />
       <div className='synthBeatOverlay__options'>
+        <div classname='synthBeatOverlay__buttonRow'>
+          
+        </div>
+        <div classname='synthBeatOverlay__durationSelect'>
+
+        </div>
 
       </div>
     </div>
